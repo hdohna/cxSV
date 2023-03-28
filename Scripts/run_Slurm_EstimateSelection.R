40bb4d470de89a9a405e8293e161176d88c6eaeb
Heinrich zu Dohna <heinrich.dohna@gmail.com>
2023-03-28 08:18:35 +0200

    Update run_Slurm_EstimateSelection.R

--- Scripts/run_Slurm_EstimateSelection.R
+++ Scripts/run_Slurm_EstimateSelection.R
@@ -5,59 +5,68 @@
 AllFunctions <- list.files('/home/hb54/cxSV/Functions/', full.names = T)
 sapply(AllFunctions, source)
 
-# Command to load r
-cmdLoadR <- 'module load R'
-cmdScript <- 'Rscript /home/hb54/cxSV/Scripts/EstimateSelectionPars_Module.R'
-
 # Specify number of SVs to sample
 NSampleSV <- 2000
+
+# Read in SV data, draw a subset and random and write it out
+SVs          <- read.delim('/home/hb54/cxSVData/simpleSV_combined_updatedinfo_AF_n_indv.txt')
+SVSampleRows <- sample(1:nrow(SVs), SampleSizeSV)
+SVSample     <- SVs[SVSampleRows, ]
+SVOutPath    <- paste0('/home/hb54/cxSVData/sampledSV', 
+                       timestamp(prefix = "_", suffix = ".csv"))
+write.csv(SVSample, SVOutPath, row.names = F)
+
+# Command to load r
+cmdLoadR  <- 'module load R'
+cmdScript <- 'Rscript /home/hb54/cxSV/Scripts/EstimateSelectionPars_Module.R'
+
 PopSize <- 5*10^3
 for (PopSize in c(5, 20) * 10^3){
   
   # Model 1: a single selection coefficient
-  ScriptFile <- '/home/hb54/cxSV/Functions/SelMod1'
-  Cmds <- c(cmdLoadR, paste(cmdScript, PopSize, NSampleSV, 0))
+  ScriptFile <- paste0('/home/hb54/cxSV/SelMod1_Pop', PopSize)
+  Cmds <- c(cmdLoadR, paste(cmdScript, PopSize, SVOutPath, 0))
   CreateAndCallSlurmScript(file = ScriptFile,
                            RunTime = '24:00:00',
-                           Mem = '20G', 
+                           Mem = '20G',
                            SlurmCommandLines =  Cmds)
-                           
+
   # Model 2: selection coefficient differs between cxSV and SV
-  ScriptFile <- '/home/hb54/cxSV/Functions/SelMod2'
-  Cmds <- c(cmdLoadR, paste(cmdScript, PopSize, NSampleSV, 1))
+  ScriptFile <- paste0('/home/hb54/cxSV/SelMod2_Pop', PopSize)
+  Cmds <- c(cmdLoadR, paste(cmdScript, PopSize, SVOutPath, 1))
   CreateAndCallSlurmScript(file = ScriptFile,
                            RunTime = '24:00:00',
-                           Mem = '20G', 
+                           Mem = '20G',
                            SlurmCommandLines =  Cmds)
-  
+
   # Model 3: selection coefficient depends on size
-  ScriptFile <- '/home/hb54/cxSV/Functions/SelMod3'
-  Cmds <- c(cmdLoadR, paste(cmdScript, PopSize, NSampleSV, 2))
+  ScriptFile <- paste0('/home/hb54/cxSV/SelMod3_Pop', PopSize)
+  Cmds <- c(cmdLoadR, paste(cmdScript, PopSize, SVOutPath, 2))
   CreateAndCallSlurmScript(file = ScriptFile,
                            RunTime = '24:00:00',
-                           Mem = '20G', 
+                           Mem = '20G',
                            SlurmCommandLines =  Cmds)
-  
+
   # Model 4: selection coefficient differs between cxSV and SV and depends on size
-  ScriptFile <- '/home/hb54/cxSV/Functions/SelMod4'
-  Cmds <- c(cmdLoadR, paste(cmdScript, PopSize, NSampleSV, '"c(1,2)"'))
+  ScriptFile <- paste0('/home/hb54/cxSV/SelMod4_Pop', PopSize)
+  Cmds <- c(cmdLoadR, paste(cmdScript, PopSize, SVOutPath, '"c(1,2)"'))
   CreateAndCallSlurmScript(file = ScriptFile,
                            RunTime = '24:00:00',
-                           Mem = '20G', 
+                           Mem = '20G',
                            SlurmCommandLines =  Cmds)
 
   # Model 5: selection coefficient differs between cxSV and SV and depends on 
   # logarithmic size
-  ScriptFile <- '/home/hb54/cxSV/Functions/SelMod5'
-  Cmds <- c(cmdLoadR, paste(cmdScript, PopSize, NSampleSV, '"c(1,3)"'))
+  ScriptFile <- paste0('/home/hb54/cxSV/SelMod5_Pop', PopSize)
+  Cmds <- c(cmdLoadR, paste(cmdScript, PopSize, SVOutPath, '"c(1,3)"'))
   CreateAndCallSlurmScript(file = ScriptFile,
                            RunTime = '24:00:00',
                            Mem = '20G', 
                            SlurmCommandLines =  Cmds)
 
   # Model 6: size dependence of selection coefficient differs between cxSV and SV
-  ScriptFile <- '/home/hb54/cxSV/Functions/SelMod6'
-  Cmds <- c(cmdLoadR, paste(cmdScript, PopSize, NSampleSV, '"c(4,5)"'))
+  ScriptFile <- paste0('/home/hb54/cxSV/SelMod6_Pop', PopSize)
+  Cmds <- c(cmdLoadR, paste(cmdScript, PopSize, SVOutPath, '"c(4,5)"'))
   CreateAndCallSlurmScript(file = ScriptFile,
                            RunTime = '24:00:00',
                            Mem = '20G', 
