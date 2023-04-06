b6c0737799cd2413d8b81b834328423f7cd0fef6
Heinrich zu Dohna <heinrich.dohna@gmail.com>
2023-04-06 08:13:47 +0200

    Update run_Slurm_EstimateSelection_InputFile.R

--- Scripts/run_Slurm_EstimateSelection_InputFile.R
+++ Scripts/run_Slurm_EstimateSelection_InputFile.R
@@ -34,16 +34,19 @@ SubmitRun <- function(PopSize, SVPath, Cols2Include, ThetaStart){
 
 # Submit runs for individual models that did not complete
 
+# linear 3-parameter model for population size of 20,000
 SubmitRun(PopSize = 2*10^4, 
           SVPath = "/home/hb54/cxSVData/sampledSV_Tue_Mar_28_08:33:50_2023.csv",
           Cols2Include = 1:2,
           ThetaStart = c(a = -0.0003921515, b = -0.0002227735, c =  -9.067496e-08))
 
+# linear 2-slope model for population size of 20,000
 SubmitRun(PopSize = 2*10^4, 
           SVPath = "/home/hb54/cxSVData/sampledSV_Tue_Mar_28_08:33:50_2023.csv",
           Cols2Include = 4:5,
           ThetaStart = c(a = -0.002144079, b = -3.404487e-07, c =  -3.0895e-07))
 
+# log-linear 3-parameter model for population size of 20,000
 SubmitRun(PopSize = 2*10^4, 
           SVPath = "/home/hb54/cxSVData/sampledSV_Tue_Mar_28_08:33:50_2023.csv",
           Cols2Include = c(1, 3),
